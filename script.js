8f5f68a8c7486785ec9df578258a2dbbac81658d
Gustavo Ferreira <gvasconcelos220@gmail.com>
2023-05-09 10:33:27 -0300

    final v1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var altura, largura
-var vidas = 1
-var tempo = 0
+var tempo = 30
+var pontuacao = 0
+var intervalId, largura, altura
 
 function tamanhoTela(){
     altura = window.innerHeight
@@ -12,16 +12,13 @@ tamanhoTela()
 function posicaoTela() {
     if(document.getElementById('bot')) {
 		document.getElementById('bot').remove()
-		if (vidas > 2) {
+		if (tempo < 0) {
 			window.location.href = "gameover.html"
-		} else {
-			document.getElementById('vida' + vidas).src = "img/heart-broken.png"
-			vidas++
 		}
 	}
 
-    var posicaoX = Math.floor(Math.random() * largura) - 90
-	var posicaoY = Math.floor(Math.random() * altura) - 90
+    var posicaoX = Math.floor(Math.random() * largura) - 180
+	var posicaoY = Math.floor(Math.random() * altura) - 250
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX
 	posicaoY = posicaoY < 0 ? 0 : posicaoY
@@ -36,9 +33,27 @@ function posicaoTela() {
 
 	bot.onclick = function () {
 		this.remove()
+		pontuacao++
+		document.getElementById('pontuacao').innerHTML = pontuacao
 	}
 
 	document.body.appendChild(bot)
 }
 
 
+
+function iniciarJogo() {
+	intervalId = setInterval(() => {
+		tempo--
+		document.getElementById('tempo').innerHTML = tempo
+		posicaoTela()
+	}, 1000)
+
+	setTimeout(function() {
+		var meuStorage = window.localStorage.setItem("minha-pontuacao", pontuacao)
+		clearInterval(intervalId)
+		window.location.href = "gameover.html"
+	}, tempo * 1000)
+}
+
+iniciarJogo()
