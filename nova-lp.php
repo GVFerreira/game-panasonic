b38edfd112e83561f0e70408f347a36d12a2b1b3
Gustavo Ferreira <gvasconcelos220@gmail.com>
2023-05-21 17:24:43 -0300

    nova lp - movendo ordem de importações

--- nova-lp.php
+++ nova-lp.php
@@ -2,11 +2,10 @@
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/nova-lp.css">
-        <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
         <title>Cadastro - Desafio V-Razor</title>
+        <link rel="stylesheet" href="./css/nova-lp.css">
     </head>
     <body>
         <header>
@@ -120,4 +119,7 @@
             echo '<script defer>alert("Já existe um cadastro com esse número de celular. Por favor, dirija-se à fila.");</script>';
         };
     ?>
+    
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 </html>
