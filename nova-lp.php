5624a270e8ddf1634ea159a0042d47a97832b1d4
Gustavo Ferreira <gvasconcelos220@gmail.com>
2023-05-21 17:05:29 -0300

    nova lp - viewport

--- nova-lp.php
+++ nova-lp.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./css/nova-lp.css">
         <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
