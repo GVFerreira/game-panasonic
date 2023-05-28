ebc65987285a9358d9358286f27a87aabbcbb44c
Gustavo Ferreira <gvasconcelos220@gmail.com>
2023-05-28 17:43:49 -0300

    ranking estático e regras sem dizeres de premiação

---
+++ ranking-estatico.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="css/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <title>Ranking</title>
+    </head>
+    <body id="body-ranking">
+        <div id="container-ranking">
+            <div id="podium">
+                <h1>Ranking</h1>
+                <div class="prisegter">
+                    <div class="seg">
+                        <h3>BELARMINO LIRA</h3>
+                        <h3>Tempo: 18.157 <span style="text-transform: lowercase">s</span></h3>
+                        <img src="./img/silver.webp" class="stage-podium" alt="">
+                    </div>
+                    
+                    <div class="pri">
+                        <h3>ARTUR LIRA</h3>
+                        <h3>Tempo: 17.533 <span style="text-transform: lowercase">s</span></h3>
+                        <img src="./img/gold.webp" class="stage-podium" alt="">
+                    </div>
+
+                    <div class="ter">
+                        <h3>JOÃO FERREIRA</h3>
+                        <h3>Tempo: 18.283 <span style="text-transform: lowercase">s</span></h3>
+                        <img src="./img/bronze.webp" class="stage-podium" alt="">
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="classificacao">
+                <div class="posicao">
+                    <div>
+                        <h2>4°</h2>
+                    </div>
+                    <div>
+                        <h2>NILO SILVA</h2>
+                    </div>
+                    <div>
+                        <h2>18.623 <span style="text-transform: lowercase; color: #001526">s</span></h2>
+                    </div>
+                </div>
+                <div class="posicao">
+                    <div>
+                        <h2>5°</h2>
+                    </div>
+                    <div>
+                        <h2>BERNARDO PORTILHO</h2>
+                    </div>
+                    <div>
+                        <h2>20.298 <span style="text-transform: lowercase; color: #001526">s</span></h2>
+                    </div>
+                </div>
+                <div class="posicao">
+                    <div>
+                        <h2>6°</h2>
+                    </div>
+                    <div>
+                        <h2>LUCAS MARANHO</h2>
+                    </div>
+                    <div>
+                        <h2>20.552 <span style="text-transform: lowercase; color: #001526">s</span></h2>
+                    </div>
+                </div>
+                <div class="posicao">
+                    <div>
+                        <h2>7°</h2>
+                    </div>
+                    <div>
+                        <h2>RAFAEL RAUGI</h2>
+                    </div>
+                    <div>
+                        <h2>20.677 <span style="text-transform: lowercase; color: #001526">s</span></h2>
+                    </div>
+                </div>
+                <div class="posicao">
+                    <div>
+                        <h2>8°</h2>
+                    </div>
+                    <div>
+                        <h2>GUSTAVO LEME</h2>
+                    </div>
+                    <div>
+                        <h2>20.867 <span style="text-transform: lowercase; color: #001526">s</span></h2>
+                    </div>
+                </div>
+                <div class="posicao">
+                    <div>
+                        <h2>9°</h2>
+                    </div>
+                    <div>
+                        <h2>HENRIQUE SOUZA</h2>
+                    </div>
+                    <div>
+                        <h2>20.979 <span style="text-transform: lowercase; color: #001526">s</span></h2>
+                    </div>
+                </div>
+                <div class="posicao">
+                    <div>
+                        <h2>10°</h2>
+                    </div>
+                    <div>
+                        <h2>FELIPE CUSNIR</h2>
+                    </div>
+                    <div>
+                        <h2>21.047 <span style="text-transform: lowercase; color: #001526">s</span></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="./img/logo-desafio.png" width="30%" style="margin-top: 50px" alt="">
+    </body>
+</html>
