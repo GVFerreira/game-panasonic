29dfd86c7d1317a322ce2595b516b6970027cb97
Gustavo Ferreira <gvasconcelos220@gmail.com>
2023-05-28 17:48:51 -0300

    atualizando o ranking para ganhadores

--- ranking-estatico.php
+++ ranking-estatico.php
@@ -10,7 +10,7 @@
     <body id="body-ranking">
         <div id="container-ranking">
             <div id="podium">
-                <h1>Ranking</h1>
+                <h1>ganhadores</h1>
                 <div class="prisegter">
                     <div class="seg">
                         <h3>BELARMINO LIRA</h3>
@@ -33,83 +33,7 @@
                 </div>
             </div>
             <div class="classificacao">
-                <div class="posicao">
-                    <div>
-                        <h2>4°</h2>
-                    </div>
-                    <div>
-                        <h2>NILO SILVA</h2>
-                    </div>
-                    <div>
-                        <h2>18.623 <span style="text-transform: lowercase; color: #001526">s</span></h2>
-                    </div>
-                </div>
-                <div class="posicao">
-                    <div>
-                        <h2>5°</h2>
-                    </div>
-                    <div>
-                        <h2>BERNARDO PORTILHO</h2>
-                    </div>
-                    <div>
-                        <h2>20.298 <span style="text-transform: lowercase; color: #001526">s</span></h2>
-                    </div>
-                </div>
-                <div class="posicao">
-                    <div>
-                        <h2>6°</h2>
-                    </div>
-                    <div>
-                        <h2>LUCAS MARANHO</h2>
-                    </div>
-                    <div>
-                        <h2>20.552 <span style="text-transform: lowercase; color: #001526">s</span></h2>
-                    </div>
-                </div>
-                <div class="posicao">
-                    <div>
-                        <h2>7°</h2>
-                    </div>
-                    <div>
-                        <h2>RAFAEL RAUGI</h2>
-                    </div>
-                    <div>
-                        <h2>20.677 <span style="text-transform: lowercase; color: #001526">s</span></h2>
-                    </div>
-                </div>
-                <div class="posicao">
-                    <div>
-                        <h2>8°</h2>
-                    </div>
-                    <div>
-                        <h2>GUSTAVO LEME</h2>
-                    </div>
-                    <div>
-                        <h2>20.867 <span style="text-transform: lowercase; color: #001526">s</span></h2>
-                    </div>
-                </div>
-                <div class="posicao">
-                    <div>
-                        <h2>9°</h2>
-                    </div>
-                    <div>
-                        <h2>HENRIQUE SOUZA</h2>
-                    </div>
-                    <div>
-                        <h2>20.979 <span style="text-transform: lowercase; color: #001526">s</span></h2>
-                    </div>
-                </div>
-                <div class="posicao">
-                    <div>
-                        <h2>10°</h2>
-                    </div>
-                    <div>
-                        <h2>FELIPE CUSNIR</h2>
-                    </div>
-                    <div>
-                        <h2>21.047 <span style="text-transform: lowercase; color: #001526">s</span></h2>
-                    </div>
-                </div>
+                
             </div>
         </div>
         <img src="./img/logo-desafio.png" width="30%" style="margin-top: 50px" alt="">
